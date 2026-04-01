8a9ae07721e3bda9a04b815813749be69e13b80e
Aditya Raj <120517393+omegaabot@users.noreply.github.com>
2026-04-01 13:45:06 +0530

    Update RunCucumberTest.java

--- src/test/java/com/demo/runners/RunCucumberTest.java
+++ src/test/java/com/demo/runners/RunCucumberTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "classpath:features",  // Changed: use classpath
         glue = { "com.demo.stepdefinitions", "com.demo.hooks" },
         plugin = {
                 "pretty",
